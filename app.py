3f3f7da4a0ebff44a52a7d09aa1c8caca5cfc35b
Richy Recarey <richyrecarey@gmail.com>
2023-05-25 22:00:32 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,38 +1,96 @@
-from collections import namedtuple
-import altair as alt
-import math
+
 import pandas as pd
 import streamlit as st
+from utils import *
+import copy
+
+
+
+def color(val):
+    color = 'orange' if val==equipo else 'white'
+    return f'color: {color}'
 
 """
-# Welcome to Streamlit!
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+# :zap: II Torneo Lóstrego :zap:
+En esta app podrás encontrar todos los horarios y resultados en tiempo real de la competición.
 
-In the meantime, below is an example of what you can do with just a few lines of code:
+Para cualquier duda, pásate por la carpa.
+
+---
 """
 
+cols_to_show = ["Hora", "Pista", "Grupo", "Equipo 1", "Equipo 2", "Resultado", "Árbitro"]
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
 
-    Point = namedtuple('Point', 'x y')
-    data = []
 
-    points_per_turn = total_points / num_turns
+df_horario = load_data()
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
 
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+equipos = pd.unique(df_horario[['Equipo 1', 'Equipo 2']].values.ravel('K')).tolist()
+equipos.insert(0, "Todos los equipos")
+equipos.insert(0, "")
+
+pistas = df_horario.Pista.dropna().unique().tolist()
+pistas.insert(0, "Todas las pistas")
+
+grupos = df_horario.Grupo.dropna().unique().tolist()
+grupos.insert(0, "Todos los grupos")
+
+competiciones = df_horario.Competición.dropna().unique().tolist()
+# competicion.insert(0, "Todas las categorías")
+
+
+competicion = st.selectbox('Competición', competiciones, key = "competicion")
+# df_horario = df_horario[(df_horario["Competición"] == competicion)]
+     
+# equipos = pd.unique(df_horario[['Equipo 1', 'Equipo 2']].values.ravel('K')).tolist()   
+# equipos.insert(0, "Todos los equipos")
+equipo = st.selectbox('Buscar tu equipo, o selecciona "Todos" para ver todo el calendario', equipos)
+
+st.markdown("---")
+
+if (equipo == equipos[0]):
+    pass
+
+elif equipo == equipos[1]:
+    # Si elegimos todo, mostramos todo!
+    st.dataframe(df_horario[cols_to_show].style.applymap(color, subset=['Equipo 1', 'Equipo 2']).applymap(bold, subset=['Equipo 1', 'Equipo 2']))
+
+else:
+    # Si se ha seleccionado algún equipo, filtramos
+    # 1. Nos quedamos solo con los partidos que juega ese equipo
+    df_horario_grupos = df_horario[(df_horario["Fase"] == "Grupos")]
+    df_horario_grupos = df_horario_grupos[(df_horario_grupos["Equipo 1"] == equipo) | (df_horario_grupos["Equipo 2"] == equipo) | (df_horario_grupos["Árbitro"] == equipo)]
+    
+
+    
+    st.caption(f'Horario para :orange[{equipo}] en el grupo N (incluye partidos a arbitrar):')
+    st.dataframe(df_horario_grupos[cols_to_show].style.applymap(color, subset=['Equipo 1', 'Equipo 2', 'Árbitro']).applymap(bold, subset=['Equipo 1', 'Equipo 2']))
+    
+    st.caption(f'Clasificación Grupo N - Pasarán a categoría oro los N primeros, y a plata los demás.')
+    st.dataframe(get_group_classification(df_horario_grupos, equipo).style.applymap(color, subset=['Equipo']))
+
+    st.caption(f'Partidos de eliminatorias para :orange[{equipo}] (incluye partidos a arbitrar):')
+    st.markdown(f':dimgray[Partidos de eliminatorias para :orange[{equipo}] (incluye partidos a arbitrar)]')
+    st.markdown(f"""<span style='color:grey'>Partidos de eliminatorias para  (incluye partidos a arbitrar)</span>""", unsafe_allow_html=True)
+    
+    df_horario_grupos = df_horario_grupos[(df_horario_grupos["Fase"] != "Grupos")]
+    df_horario_grupos = df_horario_grupos[(df_horario_grupos["Equipo 1"] == equipo) | (df_horario_grupos["Equipo 2"] == equipo) | (df_horario_grupos["Árbitro"] == equipo)]
+    
+    # Horario clasificaciones
+    df_horario_elim = df_horario[(df_horario["Fase"] != "Grupos")]
+    df_horario_elim = df_horario_elim[(df_horario_elim["Equipo 1"] == equipo) | (df_horario_elim["Equipo 2"] == equipo) | (df_horario_elim["Árbitro"] == equipo)]
+    
+    if df_horario_elim.empty:
+        st.info(f'Aún no están disponibles los partidos de clasificatoria para :orange[{equipo}]', icon="ℹ️")
+    else:
+        cols_to_show2 = ["Hora", "Pista", "Fase","Equipo 1", "Equipo 2", "Resultado", "Árbitro"]
+        st.dataframe(df_horario_elim[cols_to_show2].style.applymap(color, subset=['Equipo 1', 'Equipo 2']).applymap(bold, subset=['Equipo 1', 'Equipo 2']))
+
+
+    
+    
+    
+
